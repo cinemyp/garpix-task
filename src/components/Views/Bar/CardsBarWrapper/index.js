e26b07e56b1fb9222b283a4c0f19c1799638c30f
cinemyp <kiyamovtimur01@gmail.com>
2021-04-28 14:58:52 +0300

    finished page partners

--- src/components/Views/Bar/CardsBarWrapper/index.js
+++ src/components/Views/Bar/CardsBarWrapper/index.js
@@ -1,6 +1,8 @@
 const CardsBarWrapper = ({ children }) => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>{children}</div>
+    <div style={{ display: "flex", justifyContent: "center", gap: "40px" }}>
+      {children}
+    </div>
   );
 };
 
